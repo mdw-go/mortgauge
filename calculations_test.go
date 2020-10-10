32e04d7f284ad1e40e52df53b7466170929aa340
Michael Whatcott <mdwhatcott@gmail.com>
2020-10-09 21:33:01 -0600

    Simple tests.

--- calculations_test.go
+++ calculations_test.go
@@ -1,32 +1,34 @@
 package mortgauge
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/smartystreets/assertions/should"
-	"github.com/smartystreets/gunit"
 )
 
-func TestCalculationFixture(t *testing.T) {
-	gunit.Run(new(CalculationFixture), t)
+func TestMonthlyPayment(t *testing.T) {
+	payment := CalculateMonthlyPayment(100000, 6.0, 240)
+	assertEqual(t, 716.4310584781762, payment)
 }
 
-type CalculationFixture struct {
-	*gunit.Fixture
-}
-
-func (this *CalculationFixture) TestMonthlyPayment() {
-	this.So(
-		CalculateMonthlyPayment(100000, 6.0, 240),
-		should.AlmostEqual, 716.43, .01,
-	)
-}
-
-func (this *CalculationFixture) TestAmortizationListing() {
+func TestAmortizationListing(t *testing.T) {
 	listing := AmortizationListing(100000, 6.0, 360)
-	this.So(listing, should.HaveLength, 360)
-	this.So(listing[0].StartingPrincipal, should.Equal, 100000)
-	this.So(listing[0].MonthlyPaymentOnPrincipal, should.AlmostEqual, 99.55, .01)
-	this.So(listing[0].MonthlyPaymentOnInterest, should.AlmostEqual, 500.00, .01)
-	this.So(listing[0].RemainingPrincipal, should.AlmostEqual, 99900.45, .01)
+	if !assertEqual(t, 360, len(listing)) {
+		return
+	}
+	expected := Amortization{
+		StartingPrincipal:         100000,
+		MonthlyPaymentOnPrincipal: 99.55052515275884,
+		ExtraPaymentOnPrincipal:   0,
+		MonthlyPaymentOnInterest:  500.00,
+		RemainingPrincipal:        99900.44947484724,
+	}
+	assertEqual(t, expected, listing[0])
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}) bool {
+	if reflect.DeepEqual(expected, actual) {
+		return true
+	}
+	t.Errorf("\nExpected: %+v\nActual:   %+v", expected, actual)
+	return false
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/mdwhatcott/mortgauge
 
 go 1.14
-
-require (
-	github.com/smartystreets/assertions v1.0.1
-	github.com/smartystreets/gunit v1.1.3
-)
